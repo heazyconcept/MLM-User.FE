da7e61d51f23829436e965cc9599c32e75be4253
Oluwapelumi Odetoyinbo <odetoyinbopelumi42@gmail.com>
2026-02-23 17:48:41 +0100

    fix app update

--- mlm-user.fe/src/environments/environment.ts
+++ mlm-user.fe/src/environments/environment.ts
@@ -1,5 +1,3 @@
-// apiUrl must be set or API requests will go to the current origin.
-// On Render, set API_URL in Environment and use a build command that writes this file from process.env.
 export const environment = {
   production: true,
   apiUrl: 'https://segulah-api.onrender.com',
